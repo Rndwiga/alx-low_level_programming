ceff69030bb3907e001ca372323d541d4271f76c
Raphael Ndwiga <raphndwi@gmail.com>
2022-06-10 05:19:46 +0300

    added nested functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - writes a function that multiplies two integers
+ * @a: integer to be multiplied to b
+ * @b: integer to be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
