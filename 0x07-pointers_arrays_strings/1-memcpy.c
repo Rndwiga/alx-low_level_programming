dac392b8d47215c796f41c54b12f7e1dd973e29f
Raphael Ndwiga <raphndwi@gmail.com>
2022-06-21 01:39:10 +0300

    working with copying memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int y;
+
+	for (y = 0; y < n; y++)
+	{
+		dest[y] = src[y];
+	}
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,4 +1,4 @@
 # Working with pointers, arrays and strings
 
 * 0-memset.c - a function that fills memory with a constant byte
-*  
+* 1-memcpy.c - a function that copies memory area 
